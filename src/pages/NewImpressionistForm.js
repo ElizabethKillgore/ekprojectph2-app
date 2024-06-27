68e1f372b6d4ba8e52e022f7317e54091890327c
Elizabeth <ek@elizabethkillgore.com>
2024-06-27 14:26:06 -0400

    cleaned up code lines

--- src/pages/NewImpressionistForm.js
+++ src/pages/NewImpressionistForm.js
@@ -34,10 +34,10 @@ function NewImpressionistForm() {
         <div className="new-painting-form"  >
             <h2>New Impressionist Painting</h2>
          <form onSubmit={handleSubmit} >
-            <input type="text" artist="artist" value={artist} placeholder="Artist" onChange={(e) => setArtist(e.target.value)}/>
-            <input type="text" title="title" value={title} placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
-            <input type="text" date="date" value={date} placeholder="Date" onChange={(e) => setDate(e.target.value)}/>
-            <input type="text" image="image" value={image} placeholder="Image" onChange={(e) => setImage(e.target.value)}/>
+            <input type="text" artist="artist" value={artist} placeholder="Artist" onChange={(e) => setArtist(e.target.value)} />
+            <input type="text" title="title" value={title} placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" date="date" value={date} placeholder="Date" onChange={(e) => setDate(e.target.value)} />
+            <input type="text" image="image" value={image} placeholder="Image" onChange={(e) => setImage(e.target.value)} />
             <button type="submit">Add Painting</button>
          </form>
         </div>
